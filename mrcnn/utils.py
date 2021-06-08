1a04b4845cb79eeb776bc943361ddde996b7be10
John Polo <68556651+John-Polo@users.noreply.github.com>
2021-06-08 15:44:03 -0400

    remove add_class attribute in Dataset

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -259,20 +259,6 @@ class Dataset(object):
         self.class_info = [{"source": "", "id": 0, "name": "BG"}]
         self.source_class_ids = {}
 
-    def add_class(self, source, class_id, class_name):
-        assert "." not in source, "Source name cannot contain a dot"
-        # Does the class exist already?
-        for info in self.class_info:
-            if info['source'] == source and info["id"] == class_id:
-                # source.class_id combination already available, skip
-                return
-        # Add the class
-        self.class_info.append({
-            "source": source,
-            "id": class_id,
-            "name": class_name,
-        })
-
     def add_image(self, source, image_id, path, **kwargs):
         image_info = {
             "id": image_id,
